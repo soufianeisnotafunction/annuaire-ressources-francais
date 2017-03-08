89a078980e1ba2d395589c0ff89ea130618e9ec1
Soufiane Boutallaka <s.boutallaka@gmail.com>
2017-03-08 15:03:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <button type="button" className="btn btn-info">Info</button>
+      </div>
+    );
+  }
+}
+
+export default App;
