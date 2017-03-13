aa5ebec66f6220b4150c867bc1ff3642825c2790
paljor129 <simplonco@debian>
2017-03-13 17:13:00 +0100

    Footer

---
+++ src/Footer/footer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './footer.css';
+import FontAwesome from 'react-fontawesome'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <div className="footerMain">
+          <div className="footer1">
+            <ul className="footer1.1">
+              <li className="list"><FontAwesome name='rocket' />Contact</li>
+              <li className="list">About</li>
+              <li className="list">Help</li>
+            </ul>
+          </div>
+          <div className="footer2">
+            <ul className="footer2.1">
+              <li className="list2">Contact</li>
+              <li className="list2">About</li>
+              <li className="list2">Help</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
