90a1dead45b527cab871cb9777cde2ce7d7551cf
helene <helene.niassy@gmail.com>
2017-03-13 11:38:42 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import Cards from './components/Cards.jsx';
-import Login from './components/Login.jsx';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Cards />
-        <Login />
       </div>
     );
   }
