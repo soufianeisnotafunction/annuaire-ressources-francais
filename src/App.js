b9d8dc9e0a0fb3ed7cabd1b194f15222efbfde23
Faustino Kialungila <faustokial95@outlook.pt>
2017-03-14 15:41:41 +0100

    database

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import Menu from "./Components/Menu/Menu";
 import React, { Component } from 'react';
 import Cards from './Components/Cards.jsx';
 import Login from './Components/admin/Login';
+// import { Link} from 'react-router';
 
 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = { data:[]}
+
+  }
+  componentDidMount(){
+    fetch('http://localhost:8080/api/ress')
+            .then(result=> {
+                this.setState({data:[result.json()]});
+            });
+    }
   render() {
+console.log(this.state.data);
     return (
 
       <div className="App">
