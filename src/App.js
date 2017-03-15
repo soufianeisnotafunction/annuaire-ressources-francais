368c03c61d007a6e2146f40a0fd85030e519ec3e
paljor129 <simplonco@debian>
2017-03-15 14:23:12 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import Header from "./Components/Header/Header";
 import Menu from "./Components/Menu/Menu";
 import React, { Component } from 'react';
-<<<<<<< HEAD
+import Cards from './components/Cards.jsx';
+import Login from './components/admin/Login';
 import Footer from './Footer/footer.js';
-=======
-import Cards from './Components/Cards.jsx';
-import Login from './Components/admin/Login';
-
->>>>>>> 00d693cce8f23be29fa6f12a06641a36ed257aa5
 
 
 class App extends Component {
@@ -15,14 +11,10 @@ class App extends Component {
     return (
 
       <div className="App">
-<<<<<<< HEAD
-        <button type="button" className="btn btn-info">Info</button>
-        <Footer></Footer>
-=======
         <Header />
         <Menu />
         <Cards />
->>>>>>> 00d693cce8f23be29fa6f12a06641a36ed257aa5
+        <Footer></Footer>
       </div>
 
 
--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import './footer.css';
-<link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" integrity="sha384-dNpIIXE8U05kAbPhy3G1cz+yZmTzA6CY8Vg/u2L9xRnHjJiAK76m2BIEaSEV+/aU" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 class Footer extends Component {
   render() {
     return (
+
       <div className="footer">
         <div className="footerMain">
           <div className="footer1">
-            <ul className="footer1.1">
-              <li className="list"><FontAwesome name='contact' />Contact</li>
-              <li className="list">About</li>
-              <li className="list">Help</li>
+            <ul className="footer11">
+              <h4><b>FRENCH STATION</b></h4>
+              <li className="list0">Any other questions?</li>
+              <li className="list">+33 763528946</li>
+              <li className="list0">2017 francaise</li>
             </ul>
           </div>
           <div className="footer2">
-            <ul className="footer2.1">
-              <li className="list2"><i class="fa fa-twitter" aria-hidden="true"></i></li>
-              <li className="list2"></li>
-              <li className="list2"></li>
+            <ul className="footer21">
+              <li className="list2">Contact</li>
+              <li className="list2">About</li>
+              <li className="list2">Help</li>
+              <li className="list2">Copyright</li>
+            </ul>
+          </div>
+          <div className="footer3">
+            <ul className="footer31">
+              <li className="list3"><img className="imgFoot" src="http://www.clga.ca/sites/default/files/icons/twitter%20button.png" alt="Twitter"/></li>
+              <li className="list3"><img className="imgFoot1" src="https://www.seeklogo.net/wp-content/uploads/2016/09/facebook-icon-preview-1.png" alt="Facebook"/></li>
+              <li className="list3"><img className="imgFoot2" src="https://www.lehigh.edu/google/google_search.png" alt="Google"/></li>
             </ul>
           </div>
         </div>
