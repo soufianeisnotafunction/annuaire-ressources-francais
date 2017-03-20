f388fb5eea1398a6a98f3c83e3c4e75ac762aa16
Soufiane Boutallaka <s.boutallaka@gmail.com>
2017-03-20 14:10:50 +0100

    update delete 2

--- src/components/delete/Delete.jsx
+++ src/components/delete/Delete.jsx
@@ -14,7 +14,7 @@ class Delete extends React.Component {
     return (
         <div className=" card card-block mt-4 mb-4 col-sm-8 col-md-6 col-lg-4">
 
-            <h2 className='text-center'>{titre}</h3>
+            <h3 className='text-center'>{titre}</h3>
             <button onClick={() =>this.deleteUser()} className="btn btn-danger">Delete</button>
 
         </div>
